31a06ec7c0f64d53c7d2bbf639ce7a7b9c8c745d
aleksandr <kuzinalexandar@yandex.ru>
2025-01-30 19:03:45 +0300

    init

--- dist/bot/handlers/validateMessage.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateMessage = void 0;
-const validateMessage = () => {
-};
-exports.validateMessage = validateMessage;
