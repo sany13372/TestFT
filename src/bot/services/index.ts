09b2a09afc7abf0aebdaf005b990fc371f1c05a8
aleksandr <kuzinalexandar@yandex.ru>
2025-02-12 11:26:08 +0300

    put 20

--- src/bot/services/index.ts
+++ src/bot/services/index.ts
@@ -26,21 +26,6 @@ bot.setWebHook(webhookUrl)
     });
 
 // Обработка входящих обновлений через вебхук
-app.post(`/bot${config.BOT_TOKEN}`, (req, res) => {
-    try {
-        console.log('Полученные данные от Telegram:', JSON.stringify(req.body));
-
-        if (!req.body || Object.keys(req.body).length === 0) {
-            throw new Error('Пустой запрос или некорректные данные');
-        }
-
-        bot.processUpdate(req.body);
-        res.sendStatus(200);
-    } catch (error) {
-        console.error('Ошибка при обработке обновления:', error);
-        res.sendStatus(500);
-    }
-})
 
 bot.onText(/\/start/, startHandler(bot));
 bot.onText(/Подобрать программу для тренировки/, (msg) => workoutHandler(bot,userStates)(msg))
@@ -79,6 +64,22 @@ bot.on('message', async (msg) => {
     }
 
 });
+
+app.post(`/bot${config.BOT_TOKEN}`, (req, res) => {
+    try {
+        console.log('Полученные данные от Telegram:', JSON.stringify(req.body));
+
+        if (!req.body || Object.keys(req.body).length === 0) {
+            throw new Error('Пустой запрос или некорректные данные');
+        }
+
+        bot.processUpdate(req.body);
+        res.sendStatus(200);
+    } catch (error) {
+        console.error('Ошибка при обработке обновления:', error);
+        res.sendStatus(500);
+    }
+})
 
 app.get('/', (req, res) => {
     res.send('Сервер бота работает!');
